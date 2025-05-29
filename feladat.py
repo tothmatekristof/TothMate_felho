e9f993a35ae02b3fe1c1c2b27a1b4f66e90c8e1a
tothmatekristof <tothmatekristof@gmail.com.com>
2025-05-29 09:33:52 +0200

    Masosdik commit - kommentekkel

--- feladat.py
+++ feladat.py
@@ -1,10 +1,11 @@
+#Ezzel diagrammot keszitek napok számával és csillagok számával
 def keszit_diagram_sort(nap_szama, homerseklet):
     csillagok_szama = int(homerseklet)
     csillagok = '*' * csillagok_szama
     sor = f"Nap {nap_szama:2}: {csillagok} ({homerseklet}°C)"
     return sor
 
-
+#Ezzel diagrammot rajzolok az átlag hõmérsékletekkel
 def rajzolj_diagram(homersekletek):
     print("Napi átlaghõmérséklet diagram (°C)")
     print("-" * 40)
